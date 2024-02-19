0a4305fb4d8e327bd809b9743329dd9c8b67ed2d
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-02-19 13:36:17 -0300

    feat: completed home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,36 @@
+import { Heading, Text } from '@ignite-ui/react'
+
+import * as C from './styles'
+
+import Image from 'next/image'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <C.Container>
+      <C.Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </C.Hero>
+
+      <C.Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </C.Preview>
+    </C.Container>
+  )
+}
