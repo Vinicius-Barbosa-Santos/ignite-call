0b24665c8cfe19028426e3565a8fa5accb1e3ebc
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-02-20 19:46:07 -0300

    feat: calendar connection - calendar connection page

---
+++ src/pages/register/connect-calender/index.page.tsx
@@ -0,0 +1,41 @@
+import { ArrowRight } from 'phosphor-react'
+
+import * as C from '../styles'
+
+import * as CMain from './styles'
+
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+
+export default function Register() {
+  // const handleRegister = async (data: RegisterFormData) => {
+  // }
+
+  return (
+    <C.Container>
+      <C.Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </C.Header>
+
+      <CMain.ConnectBox>
+        <CMain.ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </CMain.ConnectItem>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </CMain.ConnectBox>
+    </C.Container>
+  )
+}
