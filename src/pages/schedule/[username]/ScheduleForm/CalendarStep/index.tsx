10322b813ce6d7bf03c7f852abdc11ad18f093a9
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-09 18:33:20 -0300

    feat: calendar and availability - Visual structure of the schedule list

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,34 @@
-import { Calendar } from '@/components/Calendar'
+import { Calendar } from '../../../../../components/Calendar'
 import * as C from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <C.Container>
+    <C.Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <C.TimePicker>
+          <C.TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </C.TimePickerHeader>
+
+          <C.TimePickerList>
+            <C.TimePickerItem>08:00h</C.TimePickerItem>
+            <C.TimePickerItem>09:00h</C.TimePickerItem>
+            <C.TimePickerItem>10:00h</C.TimePickerItem>
+            <C.TimePickerItem>11:00h</C.TimePickerItem>
+            <C.TimePickerItem>12:00h</C.TimePickerItem>
+            <C.TimePickerItem>13:00h</C.TimePickerItem>
+            <C.TimePickerItem>14:00h</C.TimePickerItem>
+            <C.TimePickerItem>15:00h</C.TimePickerItem>
+            <C.TimePickerItem>16:00h</C.TimePickerItem>
+            <C.TimePickerItem>17:00h</C.TimePickerItem>
+            <C.TimePickerItem>18:00h</C.TimePickerItem>
+          </C.TimePickerList>
+        </C.TimePicker>
+      )}
     </C.Container>
   )
 }
